93b7307231a47b82b8f5045e4f088e4c07d7d5c2
Smile Gupta <smile.gupta@razorpay.com>
2023-06-30 18:31:06 +0530

    feat: updating thr robot

--- .github/workflows/dancing-robot.js
+++ .github/workflows/dancing-robot.js
@@ -9,10 +9,7 @@ const delay = 500; // Delay between frames in milliseconds
 
 async function animate() {
   for (let i = 0; i < totalFrames; i++) {
-    if (i > 0) {
-      process.stdout.moveCursor(0, -robot[i - 1].split('\n').length);
-    }
-    process.stdout.cursorTo(0);
+    console.clear();
     console.log(robot[i]);
     await sleep(delay);
   }
