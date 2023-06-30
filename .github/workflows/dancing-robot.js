13b7f7dce72cdc8ff115685f0faa945e74df5c71
Smile Gupta <smile.gupta@razorpay.com>
2023-06-30 17:44:09 +0530

    feat: updated the dance robot

--- .github/workflows/dancing-robot.js
+++ .github/workflows/dancing-robot.js
@@ -1,6 +1,28 @@
-const dancingRobot = `
-  \\o/\\o/\\o/
-  |       |
- / \\     / \\
+const robot = `
+\\(•_•)/
+( •_•)>⌐■-■
+(⌐■_■)
 `;
-console.log(dancingRobot);
+
+const frames = robot.split('\n');
+const totalFrames = frames.length;
+const delay = 500; // Delay between frames in milliseconds
+
+async function animate() {
+  for (let i = 0; i < totalFrames; i++) {
+    console.log(frames[i]);
+    await sleep(delay);
+    clearConsole();
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function clearConsole() {
+  process.stdout.write('\u001b[3J\u001b[2J\u001b[1J');
+  process.stdout.write('\u001b[0;0H');
+}
+
+animate();
