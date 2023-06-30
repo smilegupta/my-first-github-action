35471a87448b57a9395147e660de8d57b1d4dad2
Smile Gupta <smile.gupta@razorpay.com>
2023-06-30 14:05:13 +0530

    feat: added a code for testing

---
+++ experiments.js
@@ -0,0 +1,15 @@
+function getPreferences(key) {
+  const preferences = {
+    "experiments.1cc_enable_shopify_taxes": "test",
+  };
+
+  if (key in preferences) {
+    return preferences[key];
+  } else {
+    return null;
+  }
+}
+
+export const enableShopifyTaxes = () => {
+  getPreferences("experiments.1cc_enable_shopify_taxes") === "test";
+};
